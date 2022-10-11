e270de90506e1bfdd192cf03394a13cceebe014f
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2022-10-11 17:19:50 +0100

    feat: auth

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -5,13 +5,14 @@
   const getUser = async () => {
     return await new Promise(resolve => {
       // TODO: to remove
-      setTimeout(() => {
-        resolve({
-          name: 'Guillaume',
-          user_id: 1434324334,
-          contracts_id: [2323, 3323, 434]
-        })
-      }, 0)
+      // setTimeout(() => {
+      //   resolve({
+      //     name: 'Guillaume',
+      //     user_id: 1434324334,
+      //     contracts_id: [2323, 3323, 434]
+      //   })
+      // }, 0)
+      resolve()
     })
   }
 
